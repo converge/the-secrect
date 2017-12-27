5cb3acab39e1a37d4ec172e468ffc9b8906318c9
João Vanzuita <joaopaulo.mailfs@gmail.com>
2017-12-27 18:12:40 -0200

    calculates twitter mentions volume

--- app/tweepy_listener.py
+++ app/tweepy_listener.py
@@ -1,4 +1,4 @@
-from time import sleep
+import time
 import json
 
 
@@ -6,20 +6,38 @@ class TweepyListener:
 
     def __init__(self):
         self.count = 0
+        self.tweets_peer_period = []
+        self.start_time = None
 
     def on_connect(self):
         print('Twitter logged in successfully !')
-        sleep(2)
+        time.sleep(2)
 
     def on_data(self, data):
-        print('-')
+        if self.start_time is None:
+            self.start_time = time.time()
+        else:
+            if ((time.time() - self.start_time) >= 10):
+                print('##################################################')
+                print('Number of times bitcoin word was mentioned '
+                      '(every 10 seconds) {}'.format(self.count))
+                self.start_time = None
+                self.tweets_peer_period.append(self.count)
+                for i, tweets in enumerate(self.tweets_peer_period):
+                    print('{} {}'.format(i, tweets))
+                print('##################################################')
+                time.sleep(5)
+                self.count = 0
+
         data = json.loads(data)
+        print('-')
         print(data['text'])
+        print(data['created_at'])
         print('-')
 
         self.count += 1
         print(self.count)
-        sleep(3)
+        # time.sleep(1)
         return True
 
     def on_error(self, status):
